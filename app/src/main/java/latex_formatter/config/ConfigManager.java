c899ae9b96aff82abf16d90acd59cc46777b93a9
MizukiNonoyama <germaniumdiode1139@gmail.com>
2025-10-22 10:59:58 +0900

    to place config file at jar directory not current directory.

--- app/src/main/java/latex_formatter/config/ConfigManager.java
+++ app/src/main/java/latex_formatter/config/ConfigManager.java
@@ -4,8 +4,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import latex_formatter.Main;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
 
 public final class ConfigManager {
     private static ConfigManager instance = null;
@@ -17,9 +25,19 @@ public final class ConfigManager {
     }
 
     private void load() {
+        String configDir = "";
+        try {
+            Path executionPath = getApplicationPath(Main.class);
+            String fileName = executionPath.getFileName().toFile().toString();
+            if (fileName.contains(".jar")) {
+                configDir = executionPath.getParent().toFile() + File.separator;
+            }
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try {
-            File configFile = new File("config.json");
+            File configFile = new File(configDir + "config.json");
             boolean exist = !configFile.createNewFile();
             if (!exist) {
                 this.config = new Config();
@@ -46,5 +64,14 @@ public final class ConfigManager {
             instance = new ConfigManager();
         }
         return instance;
+    }
+
+    public static Path getApplicationPath(Class<?> cls) throws URISyntaxException {
+        ProtectionDomain pd = cls.getProtectionDomain();
+        CodeSource cs = pd.getCodeSource();
+        URL location = cs.getLocation();
+        URI uri = location.toURI();
+        Path path = Paths.get(uri);
+        return path;
     }
 }
